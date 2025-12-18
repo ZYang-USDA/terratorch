c0d6e7323d1ef1017a8d68d63915851f5c1adf60
Isabelle Wittmann <isabelle.wittmann1@ibm.com>
2025-12-18 17:19:40 +0100

    remove duplicate code
    Signed-off-by: Isabelle Wittmann <isabelle.wittmann1@ibm.com>

--- terratorch/datamodules/generic_pixel_wise_data_module.py
+++ terratorch/datamodules/generic_pixel_wise_data_module.py
@@ -30,11 +30,6 @@ from .utils import check_dataset_stackability
 logger = logging.getLogger("terratorch")
 
 
-def wrap_in_compose_is_list(transform_list):
-    # set check shapes to false because of the multitemporal case
-    return A.Compose(transform_list, is_check_shapes=False) if isinstance(transform_list, Iterable) else transform_list
-
-
 # def collate_fn_list_dicts(batch):
 #     metadata = []
 #     for i in range(len(batch)):
