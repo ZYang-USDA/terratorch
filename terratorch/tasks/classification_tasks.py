57b4c0f10fd3d2e6b0674bc0d36a9ca32336e576
Isabelle Wittmann <isabelle.wittmann1@ibm.com>
2026-01-14 12:41:31 +0100

    pass model_args instead of hparams
    Signed-off-by: Isabelle Wittmann <isabelle.wittmann1@ibm.com>

--- terratorch/tasks/classification_tasks.py
+++ terratorch/tasks/classification_tasks.py
@@ -128,6 +128,7 @@ class ClassificationTask(TerraTorchTask):
 
         self.aux_loss = aux_loss
         self.aux_heads = aux_heads
+        self.model_args = model_args
 
         if model is not None and model_factory is not None:
             logger.warning("A model_factory and a model was provided. The model_factory is ignored.")
